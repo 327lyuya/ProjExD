4d212b7f00dd8474f4a9ce4fac8d3584cedcadb6
327lyuya <c0b2118609@edu.teu.ac.jp>
2022-06-21 16:53:30 +0900

    追加機能

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,26 +7,43 @@ def button_click(event):
     if num =="=":
         eqn = entry.get()
         res = eval(eqn)
-        print(res)
         entry.delete(0,tk.END)
         entry.insert(tk.END,res)
     else:
         #tkm.showinfo("",f"{num}のボタンがクリックされました")
         entry.insert(tk.END,num)
+    
+    if num == "AC":#オールクリア
+        entry.delete(0,tk.END)
+
+    #if num == "C":
+        #eqn = entry.get()
+        #entry.delete(len(eqn)-1,tk.END)[クリア機能]
+
+
+    #if num == "%":
+        #p = entry / 100
+        #entry.delete(0,tk.END)
+        #entry.insert(tk.END,p)[%機能]
+
+    #if num == "+/-":[+/-の切り替え機能]
+        
+        
 
 if __name__=="__main__":
     root = tk.Tk()
     root.title("電卓")
-    root.geometry("290x570")
+    root.geometry("392x700")
 
     entry = tk.Entry(root,
                     justify="right",
                     width=10,
                     font=("Times New Roman",40))
-    entry.grid(row=0, column=0, columnspan=3)
+    entry.grid(row=0, column=0, columnspan=4)
 
     r, c = 1, 0
-    for i, num in enumerate([9,8,7,6,5,4,3,2,1,0, "+","="]):
+    a = ["AC","C","%","/",7,8,9,"*",4,5,6,"-",1,2,3,"+",0,".","+/-","="]#四則演算、クリア機能の追加
+    for i, num in enumerate(a):
         btn = tk.Button(root,
                         text=f"{num}",
                         width=4,
@@ -36,7 +53,7 @@ if __name__=="__main__":
         btn.bind("<1>", button_click)
         btn.grid(row=r, column=c)
         c += 1
-        if (i-1) % 3 == 0:
+        if (i+1) % 4 == 0:
             r += 1
             c = 0
      
