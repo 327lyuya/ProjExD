b7029156400e2878c3feaedfeea2e533722b859a
327lyuya <c0b2118609@edu.teu.ac.jp>
2022-06-28 17:53:14 +0900

    無駄なコメントの削除 #13

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,15 +11,6 @@ def key_up(event):
 
 def main_proc():
     global mx, my
-    #delta = {#キー：押されているキーkey/値：移動幅リスト[x,y]
-     #   "": [0, 0],
-      #  "Up": [0, 20],
-       # "Down": [0, -20],
-       # "Left": [-20, 0],
-        #"Right" :[+20, 0]
-    #}
-    #cx, cy = cx+delta[key][0], cy+delta[key][0]
-    if key == "Up" and maze_bg[my-1][mx] == 0: my -= 1
     if key == "Down" and maze_bg[my+1][mx] == 0: my += 1
     if key == "Left" and maze_bg[my][mx-1] == 0: mx -= 1
     if key == "Right" and maze_bg[my][mx+1] == 0: mx += 1
